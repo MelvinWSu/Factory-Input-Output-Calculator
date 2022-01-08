e6b6db8b15b57cb4fe98907a1fd41ef7e96cb7f1
MelvinWSu <sumelvin919@gmail.com>
2022-01-07 20:09:22 -0800

    added second input

--- code/src/App.js
+++ code/src/App.js
@@ -11,8 +11,6 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
-import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 import Button from 'react-bootstrap/Button'
 
 function getFormInput() {
@@ -21,7 +19,12 @@ function getFormInput() {
   console.log(document.getElementById("input-time").value);
   console.log(document.getElementById("input-time-units").value);
   console.log(document.getElementById("input-needed").value);
-  
+
+  console.log(document.getElementById("input2-name").value);
+  console.log(document.getElementById("input2-time").value);
+  console.log(document.getElementById("input2-time-units").value);
+  console.log(document.getElementById("input2-needed").value);
+
   console.log(document.getElementById("output-name").value);
   console.log(document.getElementById("output-crafting-time").value);
   console.log(document.getElementById("output-crafting-time-units").value);
@@ -42,7 +45,7 @@ function App() {
             <form class="form-inline my-2 my-lg-0">
               <NavDropdown title="Options" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Account</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Save</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Save As</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Load</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
@@ -52,13 +55,14 @@ function App() {
 
         </Container>
       </Navbar>
-      
+
       <div class="row">
         <div class="col-12">
           <h1 class="header">Example Recipe</h1>
         </div>
       </div>
       <div class="row">
+
         <div class="col-12">
           <span class="header h4">Input #1</span>
         </div>
@@ -79,7 +83,7 @@ function App() {
             <span class="input-group-text" id="basic-addon">per</span>
             <select class="form-select" id="input-time-units">
               <option value="1">Miliseconds</option>
-              <option value="2">Seconds</option>
+              <option value="2" selected>Seconds</option>
               <option value="3">Minutes</option>
             </select>
           </div>
@@ -90,6 +94,39 @@ function App() {
             <input type="number" class="form-control" id="input-needed" aria-describedby="basic-addon3" required></input>
           </div>
         </div>
+
+        <div class="col-12">
+          <span class="header h4">Input #2</span>
+        </div>
+        <div class="col-4">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon">Name</span>
+            </div>
+            <input type="text" class="form-control" id="input2-name" aria-describedby="basic-addon3" required></input>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon">Flow</span>
+            </div>
+            <input type="number" class="form-control" id="input2-time" aria-describedby="basic-addon3" required></input>
+            <span class="input-group-text" id="basic-addon">per</span>
+            <select class="form-select" id="input2-time-units">
+              <option value="1">Miliseconds</option>
+              <option value="2" selected>Seconds</option>
+              <option value="3">Minutes</option>
+            </select>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon"># Needed</span>
+            <input type="number" class="form-control" id="input2-needed" aria-describedby="basic-addon3" required></input>
+          </div>
+        </div>
+
         <div class="col-12">
           <Button>Add Input</Button>
         </div>
@@ -115,7 +152,7 @@ function App() {
             <input type="number" class="form-control" id="output-crafting-time" aria-describedby="basic-addon3" required></input>
             <select class="form-select" id="output-crafting-time-units">
               <option value="1">Miliseconds</option>
-              <option value="2">Seconds</option>
+              <option value="2" selected>Seconds</option>
               <option value="3">Minutes</option>
             </select>
           </div>
